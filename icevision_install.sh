88662970746275bedc5bc5e7ba1b05ef7bc476de
unknown <ojassharma2003@gmail.com>
2023-05-06 17:17:12 +0530

    4

--- icevision_install.sh
+++ icevision_install.sh
@@ -13,7 +13,7 @@ case ${target} in
    cuda10)  
       echo "Installing icevision + dependencices for ${1}"
       echo "- Installing torch and its dependencies"
-      pip install torch==1.11.0+cu113 torchvision==0.11.1+cu102 torchtext==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade -q
+      pip install torch==1.10.0+cu102 torchvision==0.11.1+cu102 torchtext==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade -q
 
       echo "- Installing mmcv"
       pip install mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.10.0/index.html --upgrade -q    
@@ -22,7 +22,7 @@ case ${target} in
    cuda11)  
       echo "Installing icevision + dependencices for ${1}"
       echo "- Installing torch and its dependencies"
-      pip install torch==1.11.0+cu102 torchvision==0.12.0+cu102 torchtext --extra-index-url https://download.pytorch.org/whl/cu102 --upgrade 
+      pip install torch==1.10.0+cu111 torchvision==0.11.1+cu111 torchtext==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html --upgrade 
 
       echo "- Installing mmcv"
       pip install mmcv-full==1.3.17 -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html --upgrade -q  
